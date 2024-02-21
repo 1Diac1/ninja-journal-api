7bb480b044ebd1c86f952298a9fae6c711357be0
Ruslan <@>
2024-02-21 13:47:12 +0300

    fix: database connection string format in docker & some validation methods

--- src/Microservices/StudentsManagementService/NinjaJournal.StudentsManagement.Application/Validators/GroupDtoValidator.cs
+++ src/Microservices/StudentsManagementService/NinjaJournal.StudentsManagement.Application/Validators/GroupDtoValidator.cs
@@ -1,22 +1,33 @@
-﻿using FluentValidation;
+﻿using NinjaJournal.Microservice.Infrastructure.Abstractions.Repositories;
 using NinjaJournal.StudentsManagement.Application.Dtos;
+using NinjaJournal.StudentsManagement.Domain.Entities;
+using FluentValidation;
 
 namespace NinjaJournal.StudentsManagement.Application.Validators;
 
 public class GroupDtoValidator : AbstractValidator<GroupDto>
 {
-    public GroupDtoValidator()
+    private readonly IReadEntityRepository<Guid, Group> _readGroupEntityRepository;
+    
+    public GroupDtoValidator(IReadEntityRepository<Guid, Group> readGroupEntityRepository)
     {
+        _readGroupEntityRepository = readGroupEntityRepository;
+
         RuleFor(group => group.GroupName)
             .NotEmpty().WithMessage("{PropertyName} is required.")
-            .Length(2, 50).WithMessage("{PropertyName} must be between 2 and 50 characters.");
+            .Length(2, 50).WithMessage("{PropertyName} must be between 2 and 50 characters.")
+            .MustAsync(async (groupName, cancellationToken) => await IsGroupNameExistsAsync(groupName, cancellationToken))
+            .WithMessage("Group with this name already exists");;
         
         RuleFor(group => group.CourseNumber)
             .NotEmpty().WithMessage("{PropertyName} is required.")
             .InclusiveBetween(1, 4).WithMessage("{PropertyName} must be between 1 and 4.");
-        
-        RuleFor(group => group.Id)
-            .NotNull().WithMessage("{PropertyName} can't be null")
-            .Must(guid => guid != Guid.Empty).WithMessage("{PropertyName} can't be empty");
+    }
+    
+    private async Task<bool> IsGroupNameExistsAsync(string groupName, CancellationToken cancellationToken)
+    {
+        var group = await _readGroupEntityRepository.GetAsync(g => g.GroupName == groupName, true, cancellationToken);
+
+        return group is null;
     }
 }
